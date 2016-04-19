c50d6aa0709cf9d5761f6922762112f3482c552f
chrisntian <crc1992_3@hotmail.com>
2016-04-19 11:24:42 -0500

    add

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,77 +10,15 @@ class MovieController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *  this is a test
+     *  this is a testRRRRR
+     * @version 3.0
+     * @since    1.0
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($datos)
     {
         return "este sera la pagina de inicio";
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return "Aqui estara formulario de registro";
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 use Sami\Sami;
 use Sami\RemoteRepository\GitHubRemoteRepository;
 use Symfony\Component\Finder\Finder;
@@ -12,4 +12,34 @@ $iterator = Finder::create()
     ->in('app')
 ;
 
-return new Sami($iterator);
+return new Sami($iterator);*/
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Resources')
+    ->exclude('Tests')
+    ->in($dir = 'app')
+;
+
+// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('v3.0.*')
+    ->add('3.0', '3.0 branch')
+    ->add('master', 'master branch')
+;
+
+return new Sami($iterator, array(
+    'theme'                => 'symfony',
+    'versions'             => $versions,
+    'title'                => 'Symfony2 API',
+    'build_dir'            => __DIR__.'/../build/sf2/%version%',
+    'cache_dir'            => __DIR__.'/../cache/sf2/%version%',
+    'remote_repository'    => new GitHubRemoteRepository('crisroco/cartelera', dirname($dir)),
+    'default_opened_level' => 2,
+));
